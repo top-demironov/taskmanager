3c9c5590690b2d9a4033a8cc42af901fbd6d0ea3
Mironov Daniil <Hugant@users.noreply.github.com>
2025-04-10 15:40:36 +0700

    Update settings.py

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True  # Использование часовых поясов
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
